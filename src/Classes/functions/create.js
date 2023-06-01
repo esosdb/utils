c79b39189cbe96fff865401d5857ca95b9ed6c71
Ramco <ramazane791@gmail.com>
2023-06-01 14:36:36 +0300

    fix known bugs

--- src/Classes/functions/create.js
+++ src/Classes/functions/create.js
@@ -13,31 +13,34 @@ function createData(name, props, value, db, timestamps) {
   let target = { id: id };
   let propArr = Object.keys(crtProps);
   for (let i = 0; i < propArr.length; i++) {
-    if (crtProps[propArr[i]].required && value[propArr[i]]) {
-      if (
-        crtProps[propArr[i]].type === typeof value[propArr[i]] ||
-        Array.isArray(value[propArr[i]])
-      ) {
-        target[propArr[i]] = value[propArr[i]];
-      } else {
-        return console.error(
-          `"${propArr[i]}" must be have '${crtProps[propArr[i]].type}' type`
-        );
+    if (typeof value[propArr[i]] !== "boolean") {
+      if (crtProps[propArr[i]].required && value[propArr[i]]) {
+        if (
+          crtProps[propArr[i]].type === typeof value[propArr[i]] ||
+          Array.isArray(value[propArr[i]])
+        ) {
+          target[propArr[i]] = value[propArr[i]];
+        } else {
+          return console.error(
+            `"${propArr[i]}" must be have '${crtProps[propArr[i]].type}' type`
+          );
+        }
+      } else if (!crtProps[propArr[i]].required && value[propArr[i]]) {
+        if (
+          crtProps[propArr[i]].type === typeof value[propArr[i]] ||
+          Array.isArray(value[propArr[i]])
+        ) {
+          target[propArr[i]] = value[propArr[i]];
+        } else {
+          return console.error(
+            `"${propArr[i]}" must be have '${crtProps[propArr[i]].type}' type`
+          );
+        }
+      } else if (crtProps[propArr[i]].required && !value[propArr[i]]) {
+        return console.error(`"${propArr[i]}" has undefined value in "value"`);
       }
-    } else if (!crtProps[propArr[i]].required && value[propArr[i]]) {
-      console.log(Array.isArray(value[propArr[i]]));
-      if (
-        crtProps[propArr[i]].type === typeof value[propArr[i]] ||
-        Array.isArray(value[propArr[i]])
-      ) {
-        target[propArr[i]] = value[propArr[i]];
-      } else {
-        return console.error(
-          `"${propArr[i]}" must be have '${crtProps[propArr[i]].type}' type`
-        );
-      }
-    } else if (crtProps[propArr[i]].required && !value[propArr[i]]) {
-      return console.error(`"${propArr[i]}" has undefined value in "value"`);
+    } else {
+      target[propArr[i]] = value[propArr[i]];
     }
   }
   if (timestamps) {
