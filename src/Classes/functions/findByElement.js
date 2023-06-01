a03807d2399ffcbd8ed7fd3b250761fd870c2e69
Ramco <ramazane791@gmail.com>
2023-06-01 20:23:30 +0300

    little little fix

--- src/Classes/functions/findByElement.js
+++ src/Classes/functions/findByElement.js
@@ -2,7 +2,7 @@ function findByElement(name, element, db) {
   try {
     var property = Object.keys(element)[0];
     var value = element[property];
-    let allData = db.get(`${name}`);
+    let allData = db.get(`${name}s`);
     let foundData;
     Object.keys(allData).forEach((key) => {
       let dataElement = allData[key];
